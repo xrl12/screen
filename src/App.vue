24acfd7ef040054b0d57fcbdae0ffa6ae9f3edeb
alexxu <2996613775@qq.com>
2025-04-05 23:19:51 +0800

    feat: 添加快捷键截图

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,23 @@
 <script setup lang="ts">
 import { get_config } from '@/utils'
 import { onMounted } from 'vue'
+import type { DefaultConfig } from '../SCelectron/enum/type'
+import hotkeys from 'hotkeys-js'
+import { useRouter } from 'vue-router'
 
-onMounted(async () => {
-  const config = await get_config()
-  console.log(config)
+const router = useRouter()
+onMounted(() => {
+  get_config().then((res: DefaultConfig) => {
+    hotkeys(Object.values(res).join(','), function (_, handler) {
+      if (handler.key === res.screen_capture) {
+        router.push({ name: 'screen_capture' })
+      }
+      // console.log(event, 'event')
+      // console.log(handler, 'handler')
+      // console.log('hello world')
+    })
+  })
 })
-
 </script>
 
 <style scoped></style>
